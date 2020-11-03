0afd8e0fcba060d900dbbb2facc72a8ece9455e4
TrevorWu <wowpapa3232@gmail.com>
2020-11-03 18:58:16 +0800

    Clickable card

--- src/pages/RoadSelect.vue
+++ src/pages/RoadSelect.vue
@@ -1,10 +1,42 @@
 <template>
-  <h1>RoadSelect</h1>
+  <v-container>
+    <v-row dense>
+      <v-col cols="12" v-for="(item, i) in items" :key="i">
+        <v-card class="mx-auto" max-width="480" :color="item.color" dark link>
+          <div class="d-flex flex-no-wrap justify-space-between align-center">
+            <div>
+              <v-card-title class="headline" v-text="item.title"></v-card-title>
+              <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+            </div>
+
+            <v-avatar class="ma-3" size="100" tile>
+              <v-img :src="item.src" />
+            </v-avatar>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-
 export default {
   name: "RoadSelect",
+  data: () => ({
+    items: [
+      {
+        color: "#F573A0",
+        src: "https://image.flaticon.com/icons/svg/1087/1087436.svg",
+        title: "台 24",
+        artist: "位於屏東霧台鄉",
+      },
+      {
+        color: "#1E88E5",
+        src: "https://image.flaticon.com/icons/svg/588/588019.svg",
+        title: "182 縣道",
+        artist: "位於台南高雄交界",
+      },
+    ],
+  }),
 };
 </script>
