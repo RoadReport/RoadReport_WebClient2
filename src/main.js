a08b9e0eeab4a428a141bc6167e9a0eb450bdc73
TrevorWu <wowpapa3232@gmail.com>
2020-11-08 23:02:27 +0800

    Prepare for PWA
    - Add icons
    - Add pwa cli dependencies

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
-// import VueCookies from "vue-cookies";
+import "./registerServiceWorker";
 import vuetify from "./plugins/vuetify";
 import { firestorePlugin } from "vuefire";
 import router from "./router";
-
-// Vue.use(VueCookies);
+ 
 Vue.use(firestorePlugin);
-
-// 1. Use plugin.
-// This installs <router-view> and <router-link>,
-// and injects $router and $route to all router-enabled child components
-// Vue.use(VueRouter)
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  transpileDependencies: ["vuetify"],
+  pwa: {
+    name: "看路",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+  },
+};
