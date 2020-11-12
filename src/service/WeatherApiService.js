bddc30e5f9ab202c573d70b6b0ae34daa9b56b09
TrevorWu <wowpapa3232@gmail.com>
2020-11-12 21:41:02 +0800

    Code format

--- src/service/WeatherApiService.js
+++ src/service/WeatherApiService.js
@@ -1,10 +1,12 @@
 const axios = require("axios");
+import { remoteConfig } from "@/service/firestore";
 
-// const cors = "https://cors-anywhere.herokuapp.com/";
+remoteConfig.getValue("ANDROID_MIN_VER");
 
 export const getWeatherTemp = axios({
   method: "get",
-  url: "https://cors-anywhere.herokuapp.com/https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001",
+  url:
+    "https://cors-anywhere.herokuapp.com/https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001",
   params: {
     Authorization: "CWB-5E768A58-42AC-42E6-AE2F-078BE496737A",
     format: "JSON",
@@ -16,10 +18,9 @@ export const getWeatherTemp = axios({
     // Success
     console.log("Opendata status: " + response.status);
     console.log(response.data);
-
     return response.data;
   })
   .catch(function(error) {
     // Error
     console.log(error);
-  })
+  });
--- src/service/firestore.js
+++ src/service/firestore.js
@@ -1,17 +1,17 @@
 import firebase from "firebase/app";
 import "firebase/analytics";
-import "firebase/remote-config"
+import "firebase/remote-config";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDc9uJ-RzpHBkpXWSedJqa8UuzbidMeK5E",
-    authDomain: "roadreport-24.firebaseapp.com",
-    databaseURL: "https://roadreport-24.firebaseio.com",
-    projectId: "roadreport-24",
-    storageBucket: "roadreport-24.appspot.com",
-    messagingSenderId: "549621644976",
-    appId: "1:549621644976:web:d85562bec82925c75c4575",
-    measurementId: "G-L7H3SPSQ73",
+  apiKey: "AIzaSyDc9uJ-RzpHBkpXWSedJqa8UuzbidMeK5E",
+  authDomain: "roadreport-24.firebaseapp.com",
+  databaseURL: "https://roadreport-24.firebaseio.com",
+  projectId: "roadreport-24",
+  storageBucket: "roadreport-24.appspot.com",
+  messagingSenderId: "549621644976",
+  appId: "1:549621644976:web:d85562bec82925c75c4575",
+  measurementId: "G-L7H3SPSQ73",
 };
 
 // Initialize Firebase
@@ -20,8 +20,33 @@ firebase.initializeApp(firebaseConfig);
 // Get a Firestore instance
 export const db = firebase.firestore();
 
+export const remoteConfig = firebase.remoteConfig();
 
-const remoteConfig = firebase.remoteConfig();
 remoteConfig.settings = {
-    minimumFetchIntervalMillis: 3600000,
+  minimumFetchIntervalMillis: 3600000,
 };
+
+remoteConfig.ensureInitialized()
+  .then(() => {
+    console.log("Firebase Remote Config is initialized");
+  })
+  .catch((err) => {
+    console.error("Firebase Remote Config failed to initialize", err);
+  });
+
+remoteConfig.fetchAndActivate()
+  .then(() => {
+    console.log("fetchAndActivate Success!");
+    showMsg()
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+remoteConfig.defaultConfig = {
+  "ANDROID_MIN_VER": "999",
+};
+
+function showMsg() {
+  console.log(remoteConfig.getValue("ANDROID_MIN_VER"));
+}
