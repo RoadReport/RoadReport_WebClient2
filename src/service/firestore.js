f5b037b77c43a3f0c9d202dbe076f68252ae2606
TrevorWu <wowpapa3232@gmail.com>
2020-11-12 12:44:40 +0800

    import remote config
    and some code format

--- src/service/firestore.js
+++ src/service/firestore.js
@@ -1,14 +1,27 @@
 import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/remote-config"
 import "firebase/firestore";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDc9uJ-RzpHBkpXWSedJqa8UuzbidMeK5E",
+    authDomain: "roadreport-24.firebaseapp.com",
+    databaseURL: "https://roadreport-24.firebaseio.com",
+    projectId: "roadreport-24",
+    storageBucket: "roadreport-24.appspot.com",
+    messagingSenderId: "549621644976",
+    appId: "1:549621644976:web:d85562bec82925c75c4575",
+    measurementId: "G-L7H3SPSQ73",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 // Get a Firestore instance
-export const db = firebase.initializeApp({
-  apiKey: "AIzaSyDc9uJ-RzpHBkpXWSedJqa8UuzbidMeK5E",
-  authDomain: "roadreport-24.firebaseapp.com",
-  databaseURL: "https://roadreport-24.firebaseio.com",
-  projectId: "roadreport-24",
-  storageBucket: "roadreport-24.appspot.com",
-  messagingSenderId: "549621644976",
-  appId: "1:549621644976:web:d85562bec82925c75c4575",
-  measurementId: "G-L7H3SPSQ73",
-}).firestore();
+export const db = firebase.firestore();
+
+
+const remoteConfig = firebase.remoteConfig();
+remoteConfig.settings = {
+    minimumFetchIntervalMillis: 3600000,
+};
