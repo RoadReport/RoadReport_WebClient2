b1df636e9564386400c1e1868815e423647f393f
MrKevinLeeTW <38918101+MrKevinLeeTW@users.noreply.github.com>
2020-11-14 21:20:55 +0800

    貌似fb登入可以使用

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -9,7 +9,7 @@
             width="96"
           ></v-img>
         </v-col>
-        <v-col cols="9">
+        <v-col cols="6">
           <v-list-item two-line>
             <v-list-item-content>
               <v-list-item-title class="headline">
@@ -21,13 +21,62 @@
             </v-list-item-content>
           </v-list-item>
         </v-col>
-      </v-row>
+        <v-col cols="3">
+        <v-btn elevation="3" outlined x-large  >登入</v-btn>
+        <facebook-login class="button"
+              appId = "178598010098336"
+              @login="getUserData"
+              @logout="onLogout"
+              @get-initial-status="getUserData">
+        </facebook-login>
+        </v-col>
+      </v-row>    
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+// import Vue from "vue";
+import facebookLogin from 'facebook-login-vuejs';
+
 export default {
   name: "Login",
+
+  components: {
+    facebookLogin
+  },
+
+  methods:
+  {
+    getUserData() {
+        this.FB.api('/me', 'GET', {fields: 'id,name,email'},
+          userInformation => {
+            console.warn('data api', userInformation)
+            this.personalID = userInformation.id;
+            this.email = userInformation.email;
+            this.name = userInformation.name;
+          }
+        )
+      },
+
+      sdkLoaded(payload) {
+        this.isConnected = payload.isConnected
+        this.FB = payload.FB
+        if (this.isConnected) this.getUserData()
+      },
+
+      onLogin() {
+        this.isConnected = true
+        this.getUserData()
+      },
+
+      onLogout() {
+        this.isConnected = false;
+      },
+
+  }
+
+  
+
 };
 </script>
