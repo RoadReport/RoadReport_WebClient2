4ad1e2cfa6c6a64178efd53ba1bec455d53a7d0a
TrevorWu <wowpapa3232@gmail.com>
2020-11-14 13:10:00 +0800

    LiveCam NOW AVAILABLE

--- src/service/FirebaseRemoteConfigService.js
+++ src/service/FirebaseRemoteConfigService.js
@@ -1,4 +1,4 @@
-import { firebaseGlobal } from "@/service/firestore";
+import {firebaseGlobal} from "@/service/firestore";
 
 const remoteConfig = firebaseGlobal.remoteConfig()
 
@@ -30,7 +30,7 @@ remoteConfig.defaultConfig = {
 
 export const fetchDynamicWeatherStation = function () {
   let roadcode = localStorage.getItem("RoadCode");
-  switch (roadcode) {
+  switch (parseInt(roadcode)) {
     case 0:
       return remoteConfig.getValue("ROADCODE_24");
     case 1:
@@ -44,14 +44,17 @@ export const fetchDynamicWeatherStation = function () {
 
 export const fetchDynamicLiveCamSource = function () {
   let roadcode = localStorage.getItem("RoadCode");
-  switch (roadcode) {
+  switch (parseInt(roadcode)) {
     case 0:
+      console.log("case 0 here")
       return remoteConfig.getValue("DLCS_24");
     case 1:
+      console.log("case 1 here")
       return remoteConfig.getValue("DLCS_182");
     case 2:
       return remoteConfig.getValue("DLCS_GRANDMA");
     default:
+      console.log("case default here")
       return remoteConfig.getValue("DLCS_24");
   }
 }
