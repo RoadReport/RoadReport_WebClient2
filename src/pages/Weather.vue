135e34fd788e23b330b85d0476871ad3ec1de9f4
TrevorWu <wowpapa3232@gmail.com>
2020-11-14 15:30:29 +0800

    Weather NOW AVAILABLE

--- src/pages/Weather.vue
+++ src/pages/Weather.vue
@@ -1,28 +1,27 @@
 <template>
   <v-container>
     <v-row dense>
-       <v-col cols="12" v-for="(item, key, index) in weatherInfo" :key="item.stationId">
-<!--      <v-col cols="12">-->
+      <v-col cols="12" v-for="(item, key) in weatherInfo" :key="key">
         <v-card elevation="2" class="mx-auto" max-width="480">
           <v-row>
             <v-col cols="3">
               <v-list-item two-line>
                 <v-list-item-content>
                   <v-list-item-title class="title font-weight-regular">
-                    {{ item[0].locationName }}
+                    {{ item.locationName }}
                   </v-list-item-title>
                   <v-list-item-subtitle>
-                    {{ item[0].stationId }}
+                    {{ item.stationId }}
                   </v-list-item-subtitle>
                 </v-list-item-content>
               </v-list-item>
             </v-col>
-            <v-col cols="2"> </v-col>
+            <v-col cols="2"></v-col>
             <v-col cols="3">
               <v-list-item two-line>
                 <v-list-item-content>
                   <v-list-item-title class="title font-weight-regular">
-                    {{ item[0].weatherElement[index].elementValue }}
+                    {{ item.elementValueTemp }}
                   </v-list-item-title>
                   <v-list-item-subtitle>
                     目前溫度
@@ -34,7 +33,7 @@
               <v-list-item two-line>
                 <v-list-item-content>
                   <v-list-item-title class="title font-weight-regular">
-                    -998.00
+                    {{ item.elementValueMin10 }}
                   </v-list-item-title>
                   <v-list-item-subtitle>
                     10 分鐘雨量
@@ -50,7 +49,7 @@
 </template>
 
 <script>
-import {getWeatherTemp, getWeatherInfo} from "@/service/WeatherApiService";
+import {getWeatherInfo} from "@/service/WeatherApiService";
 
 export default {
   name: "Weather",
@@ -58,47 +57,21 @@ export default {
     weatherInfo: [],
     city: "",
   }),
-  methods: {
-    getWeather() {
-      this.$axios
-        .get(
-          "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=CWB-5E768A58-42AC-42E6-AE2F-078BE496737A&format=JSON&elementName=TEMP"
-        )
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          this.Weather.data.city = data.records.location[0].locationName;
-          this.weatherInfo = data.records.location[0].locationName;
-          this.city = "asd";
-          console.log(data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-  },
+  methods: {},
   beforeCreate() {
     if (localStorage.getItem("RoadCode") == null) {
       console.log("You fucking donkey!");
       this.$router.push("roadselect");
       console.log(
-        "%c ",
-        "font-size:800px; background:url(https://i.pinimg.com/originals/ad/fa/0c/adfa0c865a9312afea03150e1fb1cfbd.gif) no-repeat;"
+          "%c ",
+          "font-size:800px; background:url(https://i.pinimg.com/originals/ad/fa/0c/adfa0c865a9312afea03150e1fb1cfbd.gif) no-repeat;"
       );
     }
   },
   mounted() {
-    getWeatherInfo()
-    getWeatherTemp.then((response) => {
-      let temp = response.records.location[0];
-      console.log(temp.locationName + temp.stationId + temp.weatherElement[0].elementName + temp.weatherElement[0].elementValue);
-
-      this.weatherInfo = response.records;
-
-      let parsedObj = JSON.parse(JSON.stringify(this.weatherInfo));
-      console.log("parsedObj will be: " + parsedObj.location[0].locationName);
-    });
+    getWeatherInfo().then((response) => {
+      this.weatherInfo = response
+    })
   },
 };
 </script>
--- src/service/WeatherApiService.js
+++ src/service/WeatherApiService.js
@@ -2,16 +2,15 @@ import { fetchDynamicWeatherStation } from "@/service/FirebaseRemoteConfigServic
 
 const axios = require("axios");
 
-const OPENDATA_AUTH =  "CWB-5E768A58-42AC-42E6-AE2F-078BE496737A";
+const OPENDATA_AUTH = "CWB-5E768A58-42AC-42E6-AE2F-078BE496737A";
 
 function getDynamicWeatherStationJson() {
   let data = fetchDynamicWeatherStation();
   return JSON.parse(data._value);
 }
 
-function fetchWeatherData(stationID) {
-axios
-  .all([
+async function fetchWeatherData(stationID) {
+  const [firstResponse, secondResponse] = await Promise.all([
     axios.get(
       "https://cors-anywhere.herokuapp.com/https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001",
       {
@@ -33,77 +32,29 @@ axios
           stationId: stationID,
         },
       }
-    ),
-  ])
-  .then(
-    axios.spread((response1, response2) => {
-      console.log("----- I am here -----");
-      console.log(
-        "Response 1",
-        response1.data.records.location[0],
-        " Response 2",
-        response2.data.records.location[0]
-      );
+    )
+  ]);
 
-      let responseJson = {
-        locationName: response1.data.records.location[0].locationName,
-        stationId: response1.data.records.location[0].stationId,
-        elementValueTemp:
-          response1.data.records.location[0].weatherElement[0].elementValue,
-        elementValueMin10:
-          response2.data.records.location[0].weatherElement[0].elementValue,
-        elementValueRain: 999,
-      };
-
-      return responseJson;
-    })
-  )
-  .catch((error) => {
-    // Error
-    console.log(error);
-  });
+  return {
+    locationName: firstResponse.data.records.location[0].locationName,
+    stationId: firstResponse.data.records.location[0].stationId,
+    elementValueTemp:
+    firstResponse.data.records.location[0].weatherElement[0].elementValue,
+    elementValueMin10:
+    secondResponse.data.records.location[0].weatherElement[0].elementValue,
+    elementValueRain: 999,
+  };
 }
 
-export function getWeatherInfo() {
+export async function getWeatherInfo() {
   let list = getDynamicWeatherStationJson();
-  const promises = [];
-  // let data = [];
+  let data = [];
 
   for (let i = 0; i < list.length; i++) {
-    promises.push(fetchWeatherData(list[i].stationId));
+    let wow = await fetchWeatherData(list[i].stationId)
+    data.push(wow);
   }
 
-  Promise.all(promises).then((results) => {
-    console.log("Here is the results:");
-    console.log(results);
-  })
-
-  // for (let i = 0; i < list.length; i++) {
-  //   let responseJson = fetchWeatherData(list[i].stationId);
-  //   data.push(JSON.stringify(responseJson));
-  // }
-
-  // console.log(data);
+  return data
 }
 
-export const getWeatherTemp = axios({
-  method: "get",
-  url:
-    "https://cors-anywhere.herokuapp.com/https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001",
-  params: {
-    Authorization: "CWB-5E768A58-42AC-42E6-AE2F-078BE496737A",
-    format: "JSON",
-    elementName: "TEMP",
-    stationId: "C0R150",
-  },
-})
-  .then(function(response) {
-    // Success
-    console.log("Opendata status: " + response.status);
-    console.log(response.data);
-    return response.data;
-  })
-  .catch(function(error) {
-    // Error
-    console.log(error);
-  });
