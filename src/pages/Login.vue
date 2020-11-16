09e558bdccd764979b61c99d7b957209422e24a5
MrKevinLeeTW <38918101+MrKevinLeeTW@users.noreply.github.com>
2020-11-16 10:29:03 +0800

    登入登出

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -23,6 +23,7 @@
         </v-col>
         <v-col cols="3">
         <v-btn elevation="3" outlined x-large @click="socialLogin">登入</v-btn>
+        <v-btn elevation="3" outlined x-large @click="signOut">登出</v-btn>
         </v-col>
       </v-row>    
     </v-card-text>
@@ -36,15 +37,46 @@ import 'firebase/auth';
 export default {
   name: "Login",
 
+  beforeCreate: function() {
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in.
+
+        console.log(user.photoURL);
+        console.log(user.displayName);
+        console.log(user.email);
+
+      } else {
+        // User is signed out.   
+        console.log('result.user.photoURL');
+        console.log('result.user.displayName');
+        console.log('result.user.email');
+
+      }
+    })
+
+  },
+
   methods: {
     socialLogin() {
       var provider = new firebase.auth.FacebookAuthProvider();
 
       firebase.auth().signInWithPopup(provider).then((result) => {     // eslint-disable-line no-unused-vars
       
-        this.router.replace('home');
+        // this.$router.replace('Login');
         this.token = result.credential.accessToken;
         this.user = result.user;
+
+        console.log(result.user);
+
+        this.photoURL = result.user.photoURL;
+        this.displayName = result.user.displayName;
+        this.email = result.user.email;
+
+        console.log(result.user.photoURL);
+        console.log(result.user.displayName);
+        console.log(result.user.email);
 
       }).catch((err) => {
         
@@ -52,7 +84,14 @@ export default {
 
       });
 
-    }
+    },
+
+    signOut() {
+
+      firebase.auth().signOut();
+
+    },
+
   }
 
 };
