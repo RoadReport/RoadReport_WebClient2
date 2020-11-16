8f2de2c2c9a1bfc74db390fe0af3ee7156bff19b
TrevorWu <wowpapa3232@gmail.com>
2020-11-16 09:26:03 +0800

    Add Error Handling
    Some times when they updating the weather data, the weatherElement will be empty for a while.

--- src/service/WeatherApiService.js
+++ src/service/WeatherApiService.js
@@ -1,4 +1,4 @@
-import { fetchDynamicWeatherStation } from "@/service/FirebaseRemoteConfigService";
+import {fetchDynamicWeatherStation} from "@/service/FirebaseRemoteConfigService";
 
 const axios = require("axios");
 
@@ -9,7 +9,7 @@ function getDynamicWeatherStationJson() {
   return JSON.parse(data._value);
 }
 
-async function fetchWeatherData(stationID) {
+async function fetchWeatherData(stationName, stationID) {
   const [firstResponse, secondResponse] = await Promise.all([
     axios.get(
       "https://cors-anywhere.herokuapp.com/https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001",
@@ -35,13 +35,29 @@ async function fetchWeatherData(stationID) {
     )
   ]);
 
+  console.log(firstResponse.data.records);
+  console.log(secondResponse.data.records);
+
+  let firstWeatherElement = "--"
+  if (firstResponse.data.records.location.length > 0) {
+    if (firstResponse.data.records.location[0].weatherElement[0].elementValue !== undefined) {
+      firstWeatherElement = firstResponse.data.records.location[0].weatherElement[0].elementValue
+    }
+  }
+
+  let secondWeatherElement = "--"
+  if (secondResponse.data.records.location.length > 0) {
+    if (secondResponse.data.records.location[0].weatherElement[0].elementValue !== undefined) {
+      secondWeatherElement = secondResponse.data.records.location[0].weatherElement[0].elementValue
+    }
+  }
+
+
   return {
-    locationName: firstResponse.data.records.location[0].locationName,
-    stationId: firstResponse.data.records.location[0].stationId,
-    elementValueTemp:
-    firstResponse.data.records.location[0].weatherElement[0].elementValue,
-    elementValueMin10:
-    secondResponse.data.records.location[0].weatherElement[0].elementValue,
+    locationName: stationName,
+    stationId: stationID,
+    elementValueTemp: firstWeatherElement,
+    elementValueMin10: secondWeatherElement,
     elementValueRain: 999,
   };
 }
@@ -51,7 +67,7 @@ export async function getWeatherInfo() {
   let data = [];
 
   for (let i = 0; i < list.length; i++) {
-    let wow = await fetchWeatherData(list[i].stationId)
+    let wow = await fetchWeatherData(list[i].stationName, list[i].stationId)
     data.push(wow);
   }
 
