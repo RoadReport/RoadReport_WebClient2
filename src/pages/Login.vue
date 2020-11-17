3aa4ee1ec6a824875692e0215549321d37a08b08
TrevorWu <wowpapa3232@gmail.com>
2020-11-17 23:55:20 +0800

    Add links

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -14,15 +14,18 @@
           <v-btn elevation="3" color="primary" min-width="100%" @click="socialLogin" v-show="!isSignedIn">
             註冊 / 登入
           </v-btn>
-          <v-btn color="mx-auto primary" outlined @click="signOut" v-show="isSignedIn">
+          <v-btn color="primary" outlined @click="signOut" v-show="isSignedIn">
             登出
           </v-btn>
         </v-card-actions>
 
-        <v-divider class="mx-2 mt-2" v-show="!isSignedIn" />
+        <v-divider class="mx-2 mt-2" v-show="!isSignedIn"/>
 
         <v-card-text v-show="!isSignedIn">
-          註冊或登入即表示您同意我們的隱私權政策和服務條款。
+          註冊或登入即表示您同意我們的
+          <a target="_blank" href="https://bit.ly/RoadRPp">隱私權政策</a>
+          和
+          <a target="_blank" href="https://bit.ly/RoadRTos">服務條款</a>。
         </v-card-text>
       </v-card>
     </v-row>
@@ -65,7 +68,7 @@ export default {
       firebase.auth().signInWithPopup(provider).then((result) => {  // eslint-disable-line no-unused-vars
         // this.$router.go(0);
       }).catch((err) => {
-        console.log("Oops. " + err.message)
+        alert("Debug message: " + err.message)
       });
 
     },
