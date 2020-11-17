c98e0fb769afd4512a2cba3fa1e0128496c63702
TrevorWu <wowpapa3232@gmail.com>
2020-11-18 00:13:10 +0800

    Add back button to app bar at empty layout

--- src/layout/empty.vue
+++ src/layout/empty.vue
@@ -1,7 +1,11 @@
 <template>
   <v-app>
     <div>
-      <v-app-bar class="mb-2"></v-app-bar>
+      <v-app-bar class="mb-2" color="white">
+        <v-app-bar-nav-icon @click="backToHome"><v-icon>mdi-arrow-left</v-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>帳號</v-toolbar-title>
+      </v-app-bar>
+
       <v-main>
         <router-view/>
       </v-main>
@@ -9,7 +13,13 @@
   </v-app>
 </template>
 <script>
+
 export default {
   name: "Empty",
+  methods: {
+    backToHome() {
+      this.$router.push("/");
+    },
+  }
 };
 </script>
